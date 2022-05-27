668e63b7769e3be9c97bee9765e261a2c95bc9e0
pavel.milanov <pawel.milanov@yandex.ru>
2022-05-27 20:59:37 +0300

    upload Dockerfile

--- pptpd_admin/depend.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -n (sh -c "cat ../pyproject.toml | grep -n '\[tool.poetry.dependencies\]' | cut -d : -f 1", 10) ../pyproject.toml
-# cat ../pyproject.toml | grep -n '\[tool.poetry.dependencies\]' | cut -d : -f 1
-# cat ../pyproject.toml | grep -n '\[tool.poetry.dev-dependencies\]' | cut -d : -f 1
