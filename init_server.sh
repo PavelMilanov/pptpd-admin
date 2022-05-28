0a4fabd4861966a2fd0ecdc6bbe28660b7217f81
pavel.milanov <pawel.milanov@yandex.ru>
2022-05-28 18:55:36 +0300

    upload CI

--- init_server.sh
+++ init_server.sh
@@ -5,7 +5,7 @@ apt update
 apt install python3.8 python3-pip curl -y
 pip install toml
 
-# install docker
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sh get-docker.sh
-# rm get-docker.sh
+install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
