4824162b7e1d13f46eb139c95c74686e89cf4687
pavel.milanov <pawel.milanov@yandex.ru>
2022-05-29 19:08:58 +0300

    add / endpoint

--- pptpd_admin/schemas/chap_secrets.py
+++ pptpd_admin/schemas/chap_secrets.py
@@ -1,7 +1,23 @@
 from fastapi import APIRouter
+from .shell import Shell
 
 
 router = APIRouter(
     prefix='/api/v1/chap-secrets',
     tags=['chap-secrets']
 )
+
+
+@router.on_event('startup')
+async def startup_event():  # noqa: D103
+    bash = Shell()
+    bash.check_chap_secrets_file()
+
+
+@router.get('/')
+async def get_users():
+    """Read /etc/ppp/chap-secrets file.
+
+    Returns: lines file.
+    """
+    return Shell.read_chap_secrets_file()
