f5dde932ba63b3c1949b9f289e8085ec7ab3572f
pavel.milanov <pawel.milanov@yandex.ru>
2022-08-22 23:58:08 +0300

    init vue

---
+++ vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import MainView from './components/MainView.vue'
+</script>
+
+<template>
+  <MainView />
+</template>
+
+<style>
+
+</style>
---
+++ vue/src/components/MainView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <h1>hello world</h1>
+</template>
+
+<style>
+
+</style>
