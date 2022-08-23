52ed688f09926562c8f225a07710174126beae56
pavel.milanov <pawel.milanov@yandex.ru>
2022-08-23 16:57:27 +0300

    add template html

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import MainView from './components/MainView.vue'
+import Table from './components/Table.vue'
 </script>
 
 <template>
-  <MainView />
+  <Table />
 </template>
 
 <style>
---
+++ vue/src/components/Table.vue
@@ -0,0 +1,31 @@
+<script>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <table>
+    <tr>
+      <th>Client</th>
+      <th>Server</th>
+      <th>Secret</th>
+      <th>Ip</th>
+    </tr>
+    <tr>
+
+    </tr>
+  </table>
+</template>
+
+<style lang="less">
+
+table {
+  border-collapse: collapse;
+  border: 1px solid black;
+
+  th, td { 
+    border: 1px solid black;
+  }
+}
+
+</style>
