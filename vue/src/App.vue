7469c65913f6074896ebb7d6c65e52acdfaefb6b
pavel.milanov <pawel.milanov@yandex.ru>
2022-08-25 23:33:58 +0300

    create prototype web-interface

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Table from './components/Table.vue'
 </script>
 
 <template>
-  <Table />
+  <div class="container">
+    <div></div>
+    <Table />
+    <div></div>
+  </div>
 </template>
 
 <style>
--- vue/src/components/Table.vue
+++ vue/src/components/Table.vue
@@ -1,5 +1,26 @@
 <script>
 import { ref } from 'vue'
+import axios from 'axios'
+
+export default {
+  data() {
+    return { 
+      data: null  
+    }
+  },
+  methods: {
+    generateTables() {
+      axios.get(
+          'http://localhost:8000/api/v1/ppp/'  // { "3": { "client": "login", "server": "pptpd", "secret": "password", "ip": "*" }, "4": { "client": "login2", "server": "pptpd", "secret": "password2", "ip": "*" } }
+          ).then(
+              response => this.data = response.data
+          ).catch(err => {console.log(err)});
+    }
+  },
+  created() {
+    this.generateTables()
+    }
+}
 
 </script>
 
@@ -11,8 +32,17 @@ import { ref } from 'vue'
       <th>Secret</th>
       <th>Ip</th>
     </tr>
+    <tr v-for="model in data" v-bind:key="model">
+      <td>{{model.client}}</td>
+      <td>{{model.server}}</td>
+      <td>{{model.secret}}</td>
+      <td>{{model.ip}}</td>
+    </tr>
     <tr>
-
+      <td><input type="text"></td>
+      <td><input type="text"></td>
+      <td><input type="text"></td>
+      <td><input type="text"></td>
     </tr>
   </table>
 </template>
@@ -20,6 +50,7 @@ import { ref } from 'vue'
 <style lang="less">
 
 table {
+
   border-collapse: collapse;
   border: 1px solid black;
 
